aa484bf31838a2929a629d622929ea6cf3a8720d
mrl-barua <mlbarua@usep.edu.ph>
2023-08-31 08:48:26 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        
+
+
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'Home',
+    props:{
+        
+    }
+}
+</script>
+
+
+
+<style lang="">
+    
+</style>
